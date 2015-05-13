660239d27efe064e79aec945c4159148192d05b1
yankee5 <cmaverick@me.com>
2015-05-13 12:51:51 -0400

    Reward Partial and ids

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   has_many :rewards
   has_many :pledges
+  accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
 end
---
+++ db/migrate/20150513135310_add_project_i_dto_rewards.rb
@@ -0,0 +1,5 @@
+class AddProjectIDtoRewards < ActiveRecord::Migration
+  def change
+  	add_column :rewards, :project_id, :integer
+  end
+end
